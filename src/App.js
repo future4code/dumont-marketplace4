680e8a055dc1ea3c4b29d3df53e4836d35aa8fca
indiomedeiros <bboyindio@gmail.com>
2020-11-05 17:44:51 -0300

    tentativa 2 resolvendo problemas

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AppContainer } from './components/AppContainer'
+import { CartItem } from './components/CartItem'
+
+function App() {
+	return (
+		<div>
+		
+        <AppContainer />
+		<CartItem />
+		</div>
+
+	)
+}
+
+export default App
